b67e774ff1e0cb5e96474e8a04904b88682f1710
Bruno <mendesvalentim@gmail.com>
2018-03-02 17:18:39 -0400

    Correção no espaçamento

--- ExtraiMenus.py
+++ ExtraiMenus.py
@@ -2,13 +2,13 @@
 
 def executa(conexao):
     cursor = conexao.cursor()
-    cursor.execute("""SELECT IDMENU, IDMENUPARENT, DESCRICAO, TIPO,
-                    IMAGEM, IDPROGRAMA, EMPRESA, USUARIO 
-                    FROM TGERMENU ORDER BY 1""")
+    cursor.execute("""SELECT IDMENU, IDMENUPARENT, DESCRICAO, TIPO, IMAGEM, 
+                      IDPROGRAMA, EMPRESA, USUARIO 
+                      FROM TGERMENU ORDER BY 1""")
     
-    query = ("INSERT INTO TGERMENU (IDMENU, IDMENUPARENT, DESCRICAO, TIPO,"
-                                    "IMAGEM, IDPROGRAMA, EMPRESA, USUARIO)"
-                "VALUES ('{0}', '{1}', '{2}', '{3}', '{4}', '{5}', '{6}', '{7}' );\n ")
+    query = ("INSERT INTO TGERMENU (IDMENU, IDMENUPARENT, DESCRICAO, TIPO, "
+                                    "IMAGEM, IDPROGRAMA, EMPRESA, USUARIO) "
+                "VALUES ('{0}', '{1}', '{2}', '{3}', '{4}', '{5}', '{6}', '{7}');\n ")
     menus = ''
     for row in cursor:
         IDMENU, IDMENUPARENT, DESCRICAO, TIPO, IMAGEM, IDPROGRAMA, EMPRESA, USUARIO = row
